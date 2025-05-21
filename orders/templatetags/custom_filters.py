373f6d371c8a0eab01be53a848f9e95a80522c16
Zdenek Pilat <zdenek.pilat@seznam.cz>
2025-05-21 13:01:02 +0200

    změna nadpisů v navbaru, zobrazení ANO NE pro boolean pole

--- orders/templatetags/custom_filters.py
+++ orders/templatetags/custom_filters.py
@@ -2,14 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='get_attribute')
-def get_attribute(value, arg):
-    """ Získá atribut podle jeho názvu z objektu a zobrazí 'ANO' nebo 'NE' pro boolean hodnoty. """
-    result = getattr(value, arg, 'Atribut neexistuje')
-    if isinstance(result, bool):
-        return "ANO" if result else "NE"
-    return result
-
 @register.filter
 def url_remove_param(querystring, params):
     params = params.split(',')
@@ -31,4 +23,6 @@ def attr_chain(obj, attr_chain):
         obj = getattr(obj, attr, None)
         if obj is None:
             return ''
+        if isinstance(obj, bool):
+            return "ANO" if obj else "NE"        
     return obj
