f2502dc350b04c9910dfa66a0f482ef95be82e09
Zdenek Pilat <zdenek.pilat@seznam.cz>
2025-07-07 13:33:18 +0200

    Přidání testů pro utility a formuláře.

---
+++ orders/tests/tests_forms.py
@@ -0,0 +1,168 @@
+from django.test import TestCase
+from datetime import date, timedelta
+
+from orders.forms import (
+    ZakazkaAdminForm,
+    ZakazkaInlineForm,
+    BednaAdminForm,
+    VyberKamionVydejForm,
+)
+from orders.models import Zakaznik, Kamion, Zakazka, Predpis, TypHlavy, Bedna
+from orders.choices import (
+    StavBednyChoice,
+    TryskaniChoice,
+    RovnaniChoice,
+    KamionChoice,
+)
+
+
+class FormsBase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.z1 = Zakaznik.objects.create(
+            nazev="Z1", zkraceny_nazev="Z1", zkratka="Z1", ciselna_rada=100000
+        )
+        cls.z2 = Zakaznik.objects.create(
+            nazev="Z2", zkraceny_nazev="Z2", zkratka="Z2", ciselna_rada=200000
+        )
+        cls.pred1 = Predpis.objects.create(nazev="P1", skupina=1, zakaznik=cls.z1)
+        cls.pred2 = Predpis.objects.create(nazev="P2", skupina=1, zakaznik=cls.z2)
+        cls.typ = TypHlavy.objects.create(nazev="T")
+
+        # kamiony prijem
+        cls.kamion_active = Kamion.objects.create(zakaznik=cls.z1, datum=date.today())
+        cls.kamion_exped = Kamion.objects.create(zakaznik=cls.z1, datum=date.today())
+        cls.kamion_empty = Kamion.objects.create(zakaznik=cls.z1, datum=date.today())
+        cls.kamion_other = Kamion.objects.create(zakaznik=cls.z2, datum=date.today())
+
+        # zakazky pro filtr kamionu
+        Zakazka.objects.create(
+            kamion_prijem=cls.kamion_active,
+            artikl="A",
+            prumer=1,
+            delka=1,
+            predpis=cls.pred1,
+            typ_hlavy=cls.typ,
+            popis="p",
+        )
+        Zakazka.objects.create(
+            kamion_prijem=cls.kamion_exped,
+            artikl="A",
+            prumer=1,
+            delka=1,
+            predpis=cls.pred1,
+            typ_hlavy=cls.typ,
+            popis="p",
+            expedovano=True,
+        )
+        Zakazka.objects.create(
+            kamion_prijem=cls.kamion_other,
+            artikl="A",
+            prumer=1,
+            delka=1,
+            predpis=cls.pred2,
+            typ_hlavy=cls.typ,
+            popis="p",
+        )
+
+        # vydej kamiony
+        cls.kamion_vydej_ok = Kamion.objects.create(
+            zakaznik=cls.z1,
+            datum=date.today(),
+            prijem_vydej=KamionChoice.VYDEJ,
+        )
+        cls.kamion_vydej_old = Kamion.objects.create(
+            zakaznik=cls.z1,
+            datum=date.today() - timedelta(days=20),
+            prijem_vydej=KamionChoice.VYDEJ,
+        )
+        cls.kamion_vydej_other = Kamion.objects.create(
+            zakaznik=cls.z2,
+            datum=date.today(),
+            prijem_vydej=KamionChoice.VYDEJ,
+        )
+
+        cls.zakazka_edit = Zakazka.objects.create(
+            kamion_prijem=cls.kamion_active,
+            artikl="B",
+            prumer=1,
+            delka=1,
+            predpis=cls.pred1,
+            typ_hlavy=cls.typ,
+            popis="p",
+        )
+        cls.bedna = Bedna.objects.create(
+            zakazka=cls.zakazka_edit,
+            hmotnost=1,
+            tara=1,
+            stav_bedny=StavBednyChoice.K_NAVEZENI,
+            tryskat=TryskaniChoice.CISTA,
+            rovnat=RovnaniChoice.ROVNA,
+        )
+
+
+class ZakazkaPredpisValidatorTests(FormsBase):
+    def test_validator_raises_when_zakaznik_differs(self):
+        data = {
+            "kamion_prijem": self.kamion_other.pk,
+            "predpis": self.pred1.pk,
+            "artikl": "X",
+            "prumer": "1",
+            "delka": "1",
+            "typ_hlavy": self.typ.pk,
+            "popis": "p",
+        }
+        form = ZakazkaAdminForm(data)
+        self.assertFalse(form.is_valid())
+        err = form.non_field_errors()[0]
+        self.assertIn("nepatří zákazníkovi", err)
+
+
+class ZakazkaAdminFormTests(FormsBase):
+    def test_querysets_for_add(self):
+        form = ZakazkaAdminForm()
+        self.assertEqual(set(form.fields["predpis"].queryset), {self.pred1, self.pred2})
+        self.assertEqual(
+            set(form.fields["kamion_prijem"].queryset),
+            {self.kamion_active, self.kamion_other},
+        )
+
+    def test_querysets_for_change(self):
+        form = ZakazkaAdminForm(instance=self.zakazka_edit)
+        self.assertEqual(set(form.fields["predpis"].queryset), {self.pred1})
+        self.assertEqual(
+            set(form.fields["kamion_prijem"].queryset),
+            {self.kamion_active, self.kamion_other},
+        )
+
+
+class ZakazkaInlineFormTests(FormsBase):
+    def test_querysets_default_and_with_zakaznik(self):
+        form_default = ZakazkaInlineForm()
+        self.assertEqual(set(form_default.fields["predpis"].queryset), {self.pred1, self.pred2})
+
+        form_zak = ZakazkaInlineForm(zakaznik=self.z1)
+        self.assertEqual(set(form_zak.fields["predpis"].queryset), {self.pred1})
+
+    def test_queryset_for_existing_instance(self):
+        form = ZakazkaInlineForm(instance=self.zakazka_edit)
+        self.assertEqual(set(form.fields["predpis"].queryset), {self.pred1})
+
+
+class BednaAdminFormTests(FormsBase):
+    def test_initial_values(self):
+        form_new = BednaAdminForm()
+        self.assertEqual(form_new.fields["stav_bedny"].initial, StavBednyChoice.PRIJATO)
+        self.assertEqual(form_new.fields["tryskat"].initial, TryskaniChoice.NEZADANO)
+        self.assertEqual(form_new.fields["rovnat"].initial, RovnaniChoice.NEZADANO)
+
+        form_existing = BednaAdminForm(instance=self.bedna)
+        self.assertEqual(form_existing.fields["stav_bedny"].initial, self.bedna.stav_bedny)
+        self.assertEqual(form_existing.fields["tryskat"].initial, self.bedna.tryskat)
+        self.assertEqual(form_existing.fields["rovnat"].initial, self.bedna.rovnat)
+
+
+class VyberKamionVydejFormTests(FormsBase):
+    def test_queryset_filtered_by_customer_and_date(self):
+        form = VyberKamionVydejForm(zakaznik=self.z1)
+        self.assertEqual(list(form.fields["kamion"].queryset), [self.kamion_vydej_ok])
---
+++ orders/tests/tests_utils.py
@@ -0,0 +1,143 @@
+from decimal import Decimal
+from django.test import TestCase, RequestFactory
+from django.contrib.auth import get_user_model
+from django.contrib.messages.storage.fallback import FallbackStorage
+from django.contrib.messages import get_messages
+from django.http import HttpResponse
+from unittest.mock import patch
+
+from orders.utils import (
+    get_verbose_name_for_column,
+    utilita_tisk_dokumentace,
+    utilita_expedice_zakazek,
+    utilita_kontrola_zakazek,
+    utilita_zkraceni_popisu_beden,
+)
+from orders.models import Bedna, Zakazka, Kamion
+from orders.choices import StavBednyChoice, KamionChoice
+from .tests_models import ModelsBase
+from django.conf import settings
+
+
+class UtilsBase(ModelsBase):
+    """
+    Základní třída pro testy utilit v aplikaci orders.
+    Obsahuje nastavení a pomocné metody pro testování.
+    """
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.factory = RequestFactory()
+        User = get_user_model()
+        cls.user = User.objects.create_superuser('admin', 'a@example.com', 'pass')
+        settings.SECRET_KEY = 'test'
+
+    def get_request(self, method='get', data=None):
+        req = getattr(self.factory, method)('/', data or {})
+        req.user = self.user
+        req.session = {}
+        req._messages = FallbackStorage(req)
+        return req
+
+
+class GetVerboseNameTests(UtilsBase):
+    def test_nested_verbose_name(self):
+        name = get_verbose_name_for_column(
+            Bedna,
+            'zakazka__kamion_prijem__zakaznik__nazev'
+        )
+        self.assertEqual(name, 'Název zákazníka')
+        self.assertEqual(
+            get_verbose_name_for_column(Bedna, 'zakazka__kamion_prijem__datum'),
+            'Datum'
+        )
+
+
+class UtilitaTiskDokumentaceTests(UtilsBase):
+    @patch('orders.utils.render_to_string')
+    @patch('orders.utils.HTML.write_pdf')
+    def test_tisk_dokumentace(self, mock_pdf, mock_render):
+        mock_render.side_effect = ['H1', 'H2']
+        mock_pdf.return_value = b'PDF'
+        qs = Bedna.objects.all()
+        resp = utilita_tisk_dokumentace(
+            None,
+            self.get_request(),
+            qs,
+            'path.html',
+            'file.pdf'
+        )
+        self.assertIsInstance(resp, HttpResponse)
+        self.assertEqual(resp.content, b'PDF')
+        self.assertEqual(mock_render.call_count, qs.count())
+        mock_pdf.assert_called_once()
+
+
+class UtilitaExpediceZakazekTests(UtilsBase):
+    def test_expedice_beden(self):
+        self.bedna1.stav_bedny = StavBednyChoice.K_EXPEDICI
+        self.bedna1.save()
+
+        original_create = Zakazka.objects.create
+        def create_wrapper(**kwargs):
+            kwargs['predpis_id'] = kwargs.pop('predpis')
+            kwargs['typ_hlavy_id'] = kwargs.pop('typ_hlavy')
+            return original_create(**kwargs)
+
+        with patch('orders.utils.Zakazka.objects.create', side_effect=create_wrapper):
+            kamion = Kamion.objects.create(
+                zakaznik=self.zakaznik,
+                datum=self.kamion_prijem.datum,
+                prijem_vydej=KamionChoice.VYDEJ
+            )
+            qs = Zakazka.objects.filter(id=self.zakazka.id)
+            utilita_expedice_zakazek(None, self.get_request('post'), qs, kamion)
+
+            self.bedna1.refresh_from_db()
+            self.bedna2.refresh_from_db()
+            self.zakazka.refresh_from_db()
+
+            self.assertEqual(self.bedna1.stav_bedny, StavBednyChoice.EXPEDOVANO)
+            self.assertNotEqual(self.bedna2.zakazka, self.zakazka)
+            self.assertTrue(self.zakazka.expedovano)
+            self.assertEqual(self.zakazka.kamion_vydej, kamion)
+
+
+class UtilitaKontrolaZakazekTests(UtilsBase):
+    def test_no_bedny(self):
+        zak = Zakazka.objects.create(
+            kamion_prijem=self.kamion_prijem,
+            artikl='A2', prumer=Decimal('1'), delka=Decimal('1'),
+            predpis=self.predpis, typ_hlavy=self.typ_hlavy,
+            popis='p'
+        )
+        req = self.get_request('post')
+        utilita_kontrola_zakazek(None, req, Zakazka.objects.filter(id=zak.id))
+        msgs = list(get_messages(req))
+        self.assertEqual(len(msgs), 1)
+        self.assertIn('nemá žádné bedny', msgs[0].message)
+
+    def test_no_k_expedici(self):
+        req = self.get_request('post')
+        utilita_kontrola_zakazek(None, req, Zakazka.objects.filter(id=self.zakazka.id))
+        msgs = list(get_messages(req))
+        self.assertEqual(len(msgs), 1)
+        self.assertIn('nemá žádné bedny ve stavu K_EXPEDICI', msgs[0].message)
+
+    def test_pouze_komplet(self):
+        self.zakaznik.pouze_komplet = True
+        self.zakaznik.save()
+        self.bedna1.stav_bedny = StavBednyChoice.K_EXPEDICI
+        self.bedna1.save()
+        req = self.get_request('post')
+        utilita_kontrola_zakazek(None, req, Zakazka.objects.filter(id=self.zakazka.id))
+        msgs = list(get_messages(req))
+        self.assertEqual(len(msgs), 1)
+        self.assertIn('Pouze kompletní zakázky', msgs[0].message)
+
+
+class UtilitaZkraceniPopisuTests(UtilsBase):
+    def test_zkraceni_popisu(self):
+        self.zakazka.popis = 'abc def 1234 xxx'
+        utilita_zkraceni_popisu_beden(self.bedna1)
+        self.assertEqual(self.zakazka.popis, 'abc def')
